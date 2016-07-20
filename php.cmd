33fd2bd6a9f461b71082c0a7e39a544d9f6633d0
Daniel <d.sabisch@orangehive.de>
2016-07-20 14:15:45 +0200

    docker ....EXIT /B

--- php.cmd
+++ php.cmd
@@ -6,17 +6,17 @@ set CurrDirName=%CurrDirName: =%
 set CurrDirName=%CurrDirName:-=%
 
 docker exec -it %CurrDirName%_php_1 php %*
-
+EXIT /B
 
 :toLower str -- converts uppercase character to lowercase
 ::           -- str [in,out] - valref of string variable to be converted
 :$created 20060101 :$changed 20080219 :$categories StringManipulation
 :$source http://www.dostips.com
-if not defined %~1 EXIT /b
+if not defined %~1 EXIT /B
 for %%a in ("A=a" "B=b" "C=c" "D=d" "E=e" "F=f" "G=g" "H=h" "I=i"
             "J=j" "K=k" "L=l" "M=m" "N=n" "O=o" "P=p" "Q=q" "R=r"
             "S=s" "T=t" "U=u" "V=v" "W=w" "X=x" "Y=y" "Z=z" "Ä=ä"
             "Ö=ö" "Ü=ü") do (
     call set %~1=%%%~1:%%~a%%
 )
-EXIT /b
+EXIT /B
