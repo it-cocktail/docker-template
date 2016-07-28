c7602c1a41c2cab0e5dcdb5319b11bd8a31e2828
fduarte42 <duartito@gmail.com>
2016-07-28 17:52:10 +0200

    [IMPROVEMENT] - auto add local ip to docker-compose.debug.yml

--- start-debug.cmd
+++ start-debug.cmd
@@ -1,5 +1,6 @@
 :<<"::CMDLITERAL"
 @ECHO OFF
+SETLOCAL
 GOTO :CMDSCRIPT
 ::CMDLITERAL
 
@@ -10,7 +11,9 @@ if [ ! -f "$(pwd)/docker-compose.override.yml" ]; then
     cp "$(pwd)/docker-data/config-dist/docker-compose.override.yml" "$(pwd)/docker-compose.override.yml" >/dev/null
 fi
 if [ ! -f "$(pwd)/docker-compose.debug.yml" ]; then
+    LOCALIP=$(ipconfig getifaddr en0)
     cp "$(pwd)/docker-data/config-dist/docker-compose.debug.yml" "$(pwd)/docker-compose.debug.yml" >/dev/null
+    sed -i '' "s/localhost/$LOCALIP/" "$(pwd)/docker-compose.debug.yml"
 fi
 docker-compose -f docker-compose.yml -f docker-compose.override.yml -f docker-compose.debug.yml up -d --build
 exit
@@ -23,8 +26,10 @@ IF NOT EXIST "%cd%\docker-compose.override.yml" (
     COPY "%cd%\docker-data\config-dist\docker-compose.override.yml" "%cd%\docker-compose.override.yml" >NUL
 )
 IF NOT EXIST "%cd%\docker-compose.debug.yml" (
-    COPY "%cd%\docker-data\config-dist\docker-compose.debug.yml" "%cd%\docker-compose.debug.yml" >NUL
+    FOR /F "tokens=4 delims= " %%i IN ('route print ^| find " 0.0.0.0"') DO (
+        powershell -Command "(gc '%cd%\docker-data\config-dist\docker-compose.debug.yml') -replace 'localhost', '%%i' | Set-Content '%cd%\docker-compose.debug.yml'"
+    )
 )
-docker-compose -f docker-compose.yml -f docker-compose.override.yml -f docker-compose.debug.yml up -d --build
+REM docker-compose -f docker-compose.yml -f docker-compose.override.yml -f docker-compose.debug.yml up -d --build
 EXIT /B
 
