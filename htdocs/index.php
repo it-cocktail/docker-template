df28a6d73b98bcc4a30acf5dba8e5c4a8ba801e6
Tito Duarte <t.duarte@orangehive.de>
2016-08-22 00:33:05 +0100

    [FEATRURE] - added compass and compass.cmd command to use in file watcher

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,7 +1,26 @@
-<?php
+<!doctype html>
+<html>
+<head>
+    <link href="/stylesheets/screen.css" media="screen, projection" rel="stylesheet" type="text/css" />
+    <link href="/stylesheets/print.css" media="print" rel="stylesheet" type="text/css" />
+    <!--[if IE]>
+    <link href="/stylesheets/ie.css" media="screen, projection" rel="stylesheet" type="text/css" />
+    <![endif]-->
+</head>
+<body>
+    <hr />
+    <?php
 
-$result = mail('t.duarte@orangehive.de', 'docker test', 'Das ist ein Mail-Test aus Docker heraus');
+    $sent = mail('t.duarte@orangehive.de', 'docker test', 'Das ist ein Mail-Test aus Docker heraus');
 
-var_dump($result);
+    if ($sent) {
+        echo "<p style=\"text-align: center\">Test-Mail was sent</p>";
+    } else {
+        echo "<p style=\"text-align: center\">Test-Mail was not sent</p>";
+    }
+    echo "<hr />";
 
-phpinfo();
+    phpinfo();
+    ?>
+</body>
+</html>
