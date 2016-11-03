2f6c07aa3194cc667957afe40c6d27738b71bbb6
Tito Duarte <t.duarte@orangehive.de>
2016-11-03 11:35:07 +0100

    [IMPROVEMENT] - updated to new openjdk image with ant script

---
+++ bin/showlogs.cmd
@@ -0,0 +1,32 @@
+:<<"::CMDLITERAL"
+@ECHO OFF
+GOTO :CMDSCRIPT
+::CMDLITERAL
+
+OLDCWD=$(pwd)
+CWD="$( cd "$( echo "${BASH_SOURCE[0]%/*}" )" && pwd )"
+CWD=$(sed 's/.\{4\}$//' <<< "$CWD")
+cd "$CWD"
+
+docker-compose -p "${PWD##*/}" -f docker-data/config/docker-compose.yml -f docker-data/config/docker-compose.java.yml logs -f $1
+
+cd "$OLDCWD"
+exit
+
+:CMDSCRIPT
+SET OLDCWD=%cd%
+SET CWD=%~dp0
+SET CWD=%CWD:~0,-5%
+cd "%CWD%"
+
+set Projectname=%~dp0
+set Projectname=%Projectname:~0,-5%
+for %%* in (%Projectname%) do set Projectname=%%~nx*
+set Projectname=%Projectname: =%
+set Projectname=%Projectname:-=%
+set Projectname=%Projectname:.=%
+
+docker-compose -p "%Projectname%" -f docker-data/config/docker-compose.yml -f docker-data/config/docker-compose.java.yml logs -f %1
+
+CD "%OLDCWD%"
+EXIT /B
--- java-example/compile.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-exec javac HelloWorld.java
