a38a63e5e4a028d950e23a4d4122cfab39016b25
Tito Duarte <t.duarte@orangehive.de>
2017-02-18 22:48:03 +0100

    [FEATURE] major command rewrite [IMPROVEMENT] reorganizes docker-data

--- bin/_win/stop.ps1
+++ bin/_win/stop.ps1
@@ -1,7 +1,6 @@
 
 $ADDITIONAL_CONFIGFILE=""
 if (Test-Path "$env:CWD\docker-data\config\docker-compose.custom.yml") {
-    Write-Host "adding custom configuration"
     $ADDITIONAL_CONFIGFILE = "$ADDITIONAL_CONFIGFILE -f docker-data\config\docker-compose.custom.yml"
 }
 
