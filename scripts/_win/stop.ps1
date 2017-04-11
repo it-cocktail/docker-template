d63337f80cfbb32f77ceb7e9e0d440cae6d6d145
Tito Duarte <t.duarte@orangehive.de>
2017-04-11 13:57:43 +0200

    [BUGFIX] docker-compose.cml

--- scripts/_win/stop.ps1
+++ scripts/_win/stop.ps1
@@ -1,9 +1,9 @@
 
-$ADDITIONAL_CONFIGFILE=""
-if (Test-Path "$env:CWD\docker-data\config\docker-compose.custom.yml") {
-    $ADDITIONAL_CONFIGFILE = "$ADDITIONAL_CONFIGFILE -f docker-data\config\docker-compose.custom.yml"
+$ADDITIONAL_CONFIGFILE = ""
+if (Test-Path $env:CWD\docker-data\config\docker-compose.custom.yml) {
+    $ADDITIONAL_CONFIGFILE = $ADDITIONAL_CONFIGFILE + " -f docker-data\config\docker-compose.custom.yml"
 }
 
-docker-compose  -p "$env:PROJECTNAME" -f docker-data\config\base\docker-compose.yml -f docker-data\config\base\docker-compose.java.yml $ADDITIONAL_CONFIGFILE down
+Invoke-Expression "& { docker-compose -p `"$env:PROJECTNAME`" -f docker-data\config\base\docker-compose.yml $ADDITIONAL_CONFIGFILE down }"
 
 exit
