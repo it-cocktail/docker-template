7d498e2f2dcacb9ac1a798a2bfa4a897515dcb24
Tito Duarte <duartito@gmail.com>
2018-04-11 14:48:50 +0200

    [IMPROVEMENT] - prepare config for mysql 8.0 [FEATURE] - Added setup command

--- scripts/_unix/setup.sh
+++ scripts/_unix/setup.sh
@@ -17,12 +17,12 @@ if [ ! -f "$(pwd)/.env" ]; then
     while true; do
         read -p 'PROJECTNAME: ' PROJECTNAME
         sed -i "s/PROJECTNAME=.*/PROJECTNAME=$PROJECTNAME/" "$(pwd)/.env"
-    if [ "$PROJECTNAME" ]; then break; fi; done
+    if [ ! -z "$PROJECTNAME" ]; then break; fi; done
 
     php_versions=('5.3' '5.5' '5.6' '7.0' '7.1' '7.2')
     while true; do
         read -p 'PHP_VERSION (5.3, 5.5, 5.6, 7.0, 7.1, [7.2]): ' PHP_VERSION
-        if [ ! "$PHP_VERSION" ]; then
+        if [ -z "$PHP_VERSION" ]; then
             PHP_VERSION='7.2'
         fi
         sed -i "s/PHP_VERSION=.*/PHP_VERSION=$PHP_VERSION/" "$(pwd)/.env"
@@ -31,7 +31,7 @@ if [ ! -f "$(pwd)/.env" ]; then
     mysql_versions=('5.5' '5.7' '8.0')
     while true; do
         read -p 'MYSQL_VERSION (5.5, [5.7], 8.0): ' MYSQL_VERSION
-        if [ ! "$MYSQL_VERSION" ]; then
+        if [ -z "$MYSQL_VERSION" ]; then
             MYSQL_VERSION='5.7'
         fi
         sed -i "s/MYSQL_VERSION=.*/MYSQL_VERSION=$MYSQL_VERSION/" "$(pwd)/.env"
@@ -40,12 +40,12 @@ if [ ! -f "$(pwd)/.env" ]; then
     while true; do
         read -p 'MYSQL_ROOT_PASSWORD: ' MYSQL_ROOT_PASSWORD
         sed -i "s/MYSQL_ROOT_PASSWORD=.*/MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD/" "$(pwd)/.env"
-    if [ "$MYSQL_ROOT_PASSWORD" ]; then break; fi; done
+    if [ ! -z "$MYSQL_ROOT_PASSWORD" ]; then break; fi; done
 
     phpmyadmin_version=('4,6' '4.7')
     while true; do
         read -p 'PHPMYADMIN_VERSION (4.6, [4.7]): ' PHPMYADMIN_VERSION
-        if [ ! "$PHPMYADMIN_VERSION" ]; then
+        if [ -z "$PHPMYADMIN_VERSION" ]; then
             PHPMYADMIN_VERSION='4.7'
         fi
         sed -i "s/PHPMYADMIN_VERSION=.*/PHPMYADMIN_VERSION=$PHPMYADMIN_VERSION/" "$(pwd)/.env"
@@ -54,7 +54,7 @@ if [ ! -f "$(pwd)/.env" ]; then
     boolean_values=('0','1')
     while true; do
         read -p 'PHPMYADMIN_RESTRICTION (1, [0]): ' PHPMYADMIN_RESTRICTION
-        if [ ! "$PHPMYADMIN_RESTRICTION" ]; then
+        if [ -z "$PHPMYADMIN_RESTRICTION" ]; then
             PHPMYADMIN_RESTRICTION='0'
         fi
         sed -i "s/PHPMYADMIN_RESTRICTION=.*/PHPMYADMIN_RESTRICTION=$PHPMYADMIN_RESTRICTION/" "$(pwd)/.env"
@@ -63,7 +63,7 @@ if [ ! -f "$(pwd)/.env" ]; then
     boolean_values=('0','1')
     while true; do
         read -p 'AUTOPULL (1, [0]): ' AUTOPULL
-        if [ ! "$AUTOPULL" ]; then
+        if [ -z "$AUTOPULL" ]; then
             AUTOPULL='0'
         fi
         sed -i "s/AUTOPULL=.*/AUTOPULL=$AUTOPULL/" "$(pwd)/.env"
@@ -72,7 +72,7 @@ if [ ! -f "$(pwd)/.env" ]; then
     environment_values=('development' 'test' 'stage' 'production')
     while true; do
         read -p 'ENVIRONMENT ([development], test, stage, production): ' ENVIRONMENT
-        if [ ! "$ENVIRONMENT" ]; then
+        if [ -z "$ENVIRONMENT" ]; then
             ENVIRONMENT='development'
         fi
         sed -i "s/ENVIRONMENT=.*/ENVIRONMENT=$ENVIRONMENT/" "$(pwd)/.env"
@@ -80,11 +80,11 @@ if [ ! -f "$(pwd)/.env" ]; then
 
     while true; do
         read -p "BASE_DOMAIN [$PROJECTNAME.lvh.me]: " BASE_DOMAIN
-        if [ ! "$BASE_DOMAIN" ]; then
+        if [ -z "$BASE_DOMAIN" ]; then
             BASE_DOMAIN="$PROJECTNAME.lvh.me"
         fi
         sed -i "s/BASE_DOMAIN=.*/BASE_DOMAIN=$BASE_DOMAIN/" "$(pwd)/.env"
-    if [ "$BASE_DOMAIN" ] && [ "$BASE_DOMAIN" =~ \.lvh\.me$ ]; then break; fi; done
+    if [ ! -z "$BASE_DOMAIN" ] && [ "$BASE_DOMAIN" =~ \.lvh\.me$ ]; then break; fi; done
 
     while true; do
         read -p 'HTDOCS_FOLDER [htdocs] (directory must exists): ' HTDOCS_FOLDER
