4a795c945595c05aabbba9179324fa6c8d22da4c
Tito Duarte <tito.duarte@interligent.com>
2020-03-09 19:33:45 +0100

    bugfix docker-win

--- scripts/_win/composer.ps1
+++ scripts/_win/composer.ps1
@@ -1,7 +1,5 @@
 $PROJECTNAME = $envHash.PROJECTNAME
-$JSONPATH = "{range .items[?(@.metadata.name=='$PROJECTNAME-app')]}{range .status.containerStatuses[?(@.name=='php')]}{.containerID}{end}"
-$CONTAINER = (Invoke-Expression "kubectl get pods -o jsonpath=`"$JSONPATH`"") -replace 'docker://', ''
 
-Invoke-Expression "& { docker exec -it -u www-data:www-data $CONTAINER bash -l -c `"composer $args`" }"
+kubectl exec -it "$PROJECTNAME-app" --container php -- sudo -u www-data -E HOME=/var/www COMPOSER_HOME=~/.composer composer $args
 
 exit
--- scripts/_win/console.ps1
+++ scripts/_win/console.ps1
@@ -1,5 +1,5 @@
 $PROJECTNAME = $envHash.PROJECTNAME
 
-Invoke-Expression "& { kubectl exec -it "$PROJECTNAME-app" --container php -- sudo -u www-data -E HOME=/var/www bash }"
+kubectl exec -it "$PROJECTNAME-app" --container php -- sudo -u www-data -E HOME=/var/www bash
 
 exit
