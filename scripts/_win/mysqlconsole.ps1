8c778ee8221c6e219bd60e4a4eddf6aadf8b10cb
Tito Duarte <tito.duarte@interligent.com>
2020-03-09 19:35:18 +0100

    bugfix docker-win

--- scripts/_win/mysqlconsole.ps1
+++ scripts/_win/mysqlconsole.ps1
@@ -1,5 +1,5 @@
 $PROJECTNAME = $envHash.PROJECTNAME
 
-Invoke-Expression "& { kubectl exec -it "$PROJECTNAME-db-app" --container db -- bash }"
+kubectl exec -it "$PROJECTNAME-db-app" --container db -- bash
 
 exit
