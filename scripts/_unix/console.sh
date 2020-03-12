4391b3ab2e60baa3c50deaefd81333473883a634
Tito Duarte <tito.duarte@interligent.com>
2020-03-12 18:31:39 +0100

    added composer home

--- scripts/_unix/console.sh
+++ scripts/_unix/console.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-kubectl exec -it "$PROJECTNAME-app" --container php -- sudo -u www-data -E HOME=/var/www bash
+kubectl exec -it "$PROJECTNAME-app" --container php -- sudo -u www-data -E HOME=/var/www COMPOSER_HOME=/var/www/.composer bash
 
 exit
--- scripts/_win/console.ps1
+++ scripts/_win/console.ps1
@@ -1,5 +1,5 @@
 $PROJECTNAME = $envHash.PROJECTNAME
 
-kubectl exec -it "$PROJECTNAME-app" --container php -- sudo -u www-data -E HOME=/var/www bash
+kubectl exec -it "$PROJECTNAME-app" --container php -- sudo -u www-data -E HOME=/var/www COMPOSER_HOME=/var/www/.composer bash
 
 exit
