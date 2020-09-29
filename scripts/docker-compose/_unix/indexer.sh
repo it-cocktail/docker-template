11aeadcfa0b5f016f7db1380da7cb0151d393f98
Tito Duarte <duartito@gmail.com>
2020-09-29 22:19:23 +0200

    removed unneeded index commands

--- scripts/docker-compose/_unix/indexer.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-docker-compose -p "$PROJECTNAME" -f docker-compose/docker-compose.yml -f docker-compose/docker-compose.custom.yml exec search bash -l -c "indexer --all --rotate"
-
-exit
--- scripts/docker-compose/_win/indexer.ps1
+++
@@ -1,3 +0,0 @@
-docker-compose -p "$env:PROJECTNAME" -f docker-compose\docker-compose.yml -f docker-compose\docker-compose.custom.yml exec search bash -l -c "indexer --all --rotate"
-
-exit
