416be33422e1790c16cfc7b7b2ec8805e4419e64
Tito Duarte <duartito@gmail.com>
2022-02-05 10:03:36 +0100

    added apache reload

--- scripts/kubernetes/_unix/reload.sh
+++ scripts/kubernetes/_unix/reload.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-$KUBECTLCMD exec -it "app" --container php -n "$PROJECTNAME" -- apachectl gracefull
+$KUBECTLCMD exec -it "app" --container php -n "$PROJECTNAME" -- apachectl graceful
 
 exit
